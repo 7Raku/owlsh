172d7b822f84b4d663a52bd9684b4d93041c6753
Raku <161195773+7Raku@users.noreply.github.com>
2026-07-15 21:26:34 +0200

    feat: add basic read-eval-print loop skeleton

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,20 @@
 const std = @import("std");
 
-pub fn main() !void {
-    std.debug.print("Hello, owlsh!", .{});
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+
+    var stdin_buf: [1024]u8 = undefined;
+    var stdin_file = std.Io.File.stdin();
+    var stdin_reader = stdin_file.reader(io, &stdin_buf);
+
+    const stdin = &stdin_reader.interface;
+
+    while (true) {
+        std.debug.print("> ", .{});
+        const bare_line = try stdin.takeDelimiter('\n');
+        const line = bare_line orelse break;
+        const clean_line = std.mem.trim(u8, line, "\r");
+
+        std.debug.print("{s}\n", .{clean_line});
+    }
 }
