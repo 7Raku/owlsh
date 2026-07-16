5632ccdf541b29ca20b23932cc0d8c5e80fd70c2
Raku <161195773+7Raku@users.noreply.github.com>
2026-07-16 14:23:14 +0200

    fix: write shell output to stdout instead of stderr

--- src/main.zig
+++ src/main.zig
@@ -6,15 +6,22 @@ pub fn main(init: std.process.Init) !void {
     var stdin_buf: [1024]u8 = undefined;
     var stdin_file = std.Io.File.stdin();
     var stdin_reader = stdin_file.reader(io, &stdin_buf);
-
     const stdin = &stdin_reader.interface;
 
+    var stdout_buf: [1024]u8 = undefined;
+    var stdout_file = std.Io.File.stdout();
+    var stdout_writer = stdout_file.writer(io, &stdout_buf);
+    const stdout = &stdout_writer.interface;
+
     while (true) {
-        std.debug.print("> ", .{});
+        try stdout.writeAll("> ");
+        try stdout.flush();
+
         const bare_line = try stdin.takeDelimiter('\n');
         const line = bare_line orelse break;
         const clean_line = std.mem.trim(u8, line, "\r");
 
-        std.debug.print("{s}\n", .{clean_line});
+        try stdout.print("{s}\n", .{clean_line});
+        try stdout.flush();
     }
 }
