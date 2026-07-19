2906b479d66b11dc1018d12069830c8f78908b50
Raku <161195773+7Raku@users.noreply.github.com>
2026-07-19 17:37:57 +0200

    feat: colorize error messages

--- src/output.zig
+++ src/output.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
 
+const color_err = "\x1b[31m";
+const color_reset = "\x1b[0m";
+
 pub fn printError(stdout: *std.Io.Writer, source: []const u8, comptime fmt: []const u8, args: anytype) !void {
-    try stdout.print("{s}: ", .{source});
+    try stdout.print("{s}{s}: ", .{ color_err, source });
     try stdout.print(fmt, args);
-    try stdout.print("\n", .{});
+    try stdout.print("{s}\n", .{color_reset});
     try stdout.flush();
 }
